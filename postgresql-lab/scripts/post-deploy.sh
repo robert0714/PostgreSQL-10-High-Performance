5c9d71d9390bf30800c58623367ac6d653787159
robert.lee <robert0714@gmail.com>
2019-09-05 18:08:03 +0800

    add samples

---
+++ postgresql-lab/scripts/post-deploy.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+# centos 7.6
+value=$( grep -ic "entry" /etc/hosts )
+if [ $value -eq 0 ]
+then
+echo "
+################ hadoop-cookbook host entry ############
+100.100.100.101  node1
+100.100.100.102  node2 
+######################################################
+" > /etc/hosts
+fi
+sudo  /usr/pgsql-11/bin/postgresql-11-setup initdb
+
+
+sudo sh -c 'echo host  all  all  0.0.0.0/0  md5  >>  /var/lib/pgsql/11/data/pg_hba.conf'
+sudo sh -c 'echo listen_addresses = \'*\'   >>  /var/lib/pgsql/11/data/postgresql.conf'
+
+sudo systemctl enable postgresql-11.service
+sudo systemctl start postgresql-11.service
+
+
